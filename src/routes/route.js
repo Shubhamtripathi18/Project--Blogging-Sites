625876f7371b60d2ba7400970d8ce00e6f69bc92
Shubham Tripathi <99880868+Shubhamtripathi18@users.noreply.github.com>
2022-03-21 14:35:36 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,11 +4,13 @@ const mainController= require("../controller/mainController.js");
 const { middleware } = require('../middleWare/middleware.js');
 
 router.post("/authors",mainController.createAuthor);
-router.post("/blogs",middleware,mainController.createBlog);
+router.post("/login",mainController.login);
+
+
+router.post("/blogs",middleware,mainController.createBlog); 
 router.get("/getblog",middleware,mainController.getFilteredBlogs);
 router.put("/updateBlog/:blogId",middleware,mainController.updateBlog);
 router.delete("/deleteblogs/:blogId",middleware,mainController.deleteBlog);
-router.delete("/deleteblogsbyquery", middleware,mainController.queryParamsDelete);
-router.post("/login",mainController.login);
-module.exports=router;
+router.delete("/deleteblogsbyquery",middleware, mainController.queryParamsDelete); /// all apis working fine
 
+module.exports=router;
