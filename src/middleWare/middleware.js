e7487528bfd8a9754ec5d8eab7143156172af776
Shubham Tripathi <99880868+Shubhamtripathi18@users.noreply.github.com>
2022-03-21 14:36:19 +0530

    Update middleware.js

--- src/middleWare/middleware.js
+++ src/middleWare/middleware.js
@@ -3,17 +3,13 @@ const jwt = require("jsonwebtoken");
 const middleware = async function (req, res, next) {
     //Authorization..
     try {
-        let authorId = req.params.authorId;
-        if (!authorId) {
-            return authorId = req.query.authorId;
-        }
         let tokenData = req.headers["x-auth-token"];
         if (!tokenData) {
-            res.status(400).send({ msg: "No token Data" });
+            return res.status(400).send({ msg: "No token Data" });
         }
 
         let verifyUser = jwt.verify(tokenData, "secretKeyforAuthor")
-        res.send(verifyUser)   //--> Userid of the author
+        // res.send(verifyUser)   //--> Userid of the author
         if (verifyUser) {
             req.validate = verifyUser;
             next()
@@ -22,8 +18,9 @@ const middleware = async function (req, res, next) {
             return res.status(400).send({ status: false, msg: "Invalid token" })
         }
     }catch(err){
-        res.statsu(500).send({status:false,msg:"Error Found"})
+        res.status(500).send({status:false,msg:"Error Found"})
     }
 }
+
 
 module.exports.middleware = middleware
